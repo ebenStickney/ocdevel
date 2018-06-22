f6c1f1c0595af91b8d95162d6d5fb773f0832619
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-06-22 13:48:46 -0700

    creating dynamic responsive collapse

--- new/src/components/Dashboard.js
+++ new/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { CardStack, Card } from 'react-cardstack';
 
 class DashboardPage extends React.Component {
   state = {
@@ -23,6 +23,7 @@ class DashboardPage extends React.Component {
                     <h2 className="stacked__name">Machine Learning Developer</h2>
                     <h2>Creator of the Machine Learning Guide Podcast</h2>
                 </div>
+
           </div>
           <div className="side-container card-container">
               {this.state.hasClicked ?
@@ -82,45 +83,59 @@ class DashboardPage extends React.Component {
                     <h1> Highlight Reel </h1>
                   </div>
                   <div className="project-container">
-                    <div className="project">
-                      <img src="/images/MLG-logo-80.jpg" alt="MLG Logo" />
-                      <div className="blurb-container">
-                        <h3>Machine Learning Podcast</h3>
-                        <p className="blurb">
-                          This series aims to teach you the high level fundamentals of machine learning
-                          from A to Z. We discuss languages and frameworks, deep learning, and more.
-                          At the end of every episode I’ll provide the best-of-the-best resources curated from around the web for
-                          you to learn each episode’s details.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="project">
-                      <img src="/images/habitica-logo-80.png" alt="Habitica Logo" />
-                      <div className="blurb-container">
-                        <h3>Habitica</h3>
-                        <p className="blurb">
-                           I built the website (Angular, Node / Express, Mongo),
-                           mobile apps (Ionic / Cordova, iOS, Android), and Chrome Extension.
-                           Managed dev-ops on Heroku and Amazon Web Services.
-                           This was a deep-dive into scalability, marketing, fundraising, hiring, and more.
-                           I managed an open source community of 200+ contributors (github.com/habitrpg/habitrpg).
-                           Was a one-man-band running this 800k+ user-base for 1.5y before hiring.
-                           Lots of startup lessons to bear!
-                        </p>
-                      </div>
-                    </div>
-                    <div className="project">
-                      <img src="/images/OCDevel-logo-sq-80.jpg" alt="OCDevel Logo" />
-                        <div className="blurb-container OCD">
-                        <h3>Other OCDevel Projects</h3>
-                        <ul className="blurb">
-                          <li>Bitcoin Trading Bot </li>
-                          <li>ExecVision</li>
-                          <li>Jobpig</li>
-                          <li>Humana</li>
-                        </ul>
-                      </div>
-                    </div>
+                    <CardStack
+                    	height={400}
+                    	width={500}
+                    	background='#0c1720'
+                    	hoverOffset={25}>
+
+                    	<Card background='#0c1720'>
+                        <div className="project">
+                          <img src="/images/MLG-logo-80.jpg" alt="MLG Logo" />
+                          <div className="blurb-container">
+                            <h3>Machine Learning Podcast</h3>
+                            <p className="blurb">
+                              This series aims to teach you the high level fundamentals of machine learning
+                              from A to Z. We discuss languages and frameworks, deep learning, and more.
+                              At the end of every episode I’ll provide the best-of-the-best resources curated from around the web for
+                              you to learn each episode’s details.
+                            </p>
+                          </div>
+                        </div>
+                    	</Card>
+
+                    	<Card background='#0c1720'>
+                        <div className="project">
+                          <img src="/images/habitica-logo-80.png" alt="Habitica Logo" />
+                          <div className="blurb-container">
+                            <h3>Habitica</h3>
+                            <p className="blurb">
+                               I built the website (Angular, Node / Express, Mongo),
+                               mobile apps (Ionic / Cordova, iOS, Android), and Chrome Extension.
+                               Managed dev-ops on Heroku and Amazon Web Services.
+                               This was a deep-dive into scalability, marketing, fundraising, hiring, and more.
+                               I managed an open source community of 200+ contributors (github.com/habitrpg/habitrpg).
+                               Was a one-man-band running this 800k+ user-base for 1.5y before hiring.
+                               Lots of startup lessons to bear!
+                            </p>
+                          </div>
+                        </div>
+                    	</Card>
+                      <Card background='#0c1720'>
+                        <div className="project">
+                          <img src="/images/OCDevel-logo-sq-80.jpg" alt="OCDevel Logo" />
+                            <div className="blurb-container OCD">
+                            <h3>Other OCDevel Projects</h3>
+                            <ul className="blurb">
+                              <li>Bitcoin Trading Bot </li>
+                              <li>ExecVision</li>
+                              <li>Jobpig</li>
+                              <li>Humana</li>
+                            </ul>
+                          </div>
+                        </div>
+                    	</Card>
+                    </CardStack>
                   </div>
 
 
@@ -133,6 +148,8 @@ class DashboardPage extends React.Component {
       </div>
     )};
   };
+
+
 
 
 // const DashboardPage = () => (

14a17322db7a0a9f9ac9646893143cd20f273248
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-08-30 18:38:29 -0700

    new stack collapsible

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
+
+
+const projects = [
+  {
+    title: "Machine Learning Guide Podcast >",
+    image: "/images/MLG-logo-80.jpg",
+    link: "/mlg",
+    blurb: "This series aims to teach you the high level fundamentals of machine learning from A to Z. We discuss languages and frameworks, deep learning, and more. At the end of every episode I’ll provide the best-of-the-best resources curated from around the web for you to learn each episode’s details."
+  },
+  {
+    title: "Habitica >",
+    image: "/images/habitica-logo-80.png",
+    link: "https://habitica.com/",
+    blurb: "I built the website, mobile apps, and Chrome Extension. Managed dev-ops on Heroku and Amazon Web Services. This was a deep-dive into scalability, marketing, fundraising, hiring, and more. I managed an open source community of 200+ contributors. Was a one-man-band running this 800k+ user-base for 1.5y before hiring."
+  }
+]
 
 
 
@@ -12,7 +29,24 @@ const Projects = (props) => (
       <h1> Highlight Reel </h1>
     </div>
     <div className="project-container">
-        Filler
+        {projects.map(project => {
+          return (
+            <Collapsible trigger={project.title}>
+              <a href={project.link} target="_blank"><img src={project.image} alt={project.title} /></a>
+              <p className='blurb'>{project.blurb}</p>
+            </Collapsible>
+          )
+        })}
+        <Collapsible trigger="Other Projects >">
+          <img src="/images/OCDevel-logo-sq-80.jpg" alt="OCDevel" />
+          <p className='blurb'>
+            -Bitcoin Trading Bot <br />
+            -ExecVision <br />
+            -Jobpig <br />
+            -Humana <br />
+          </p>
+        </Collapsible>
+
     </div>
   </div>
 );
@@ -20,78 +54,30 @@ const Projects = (props) => (
 export default Projects;
 
 
-//there is a bug that developer needs to fix, leaving this off for now.
-// import { CardStack, Card } from 'react-cardstack';
-// <CardStack
-//   height={400}
-//   width={350}
-//   background='#0c1720'
-//   hoverOffset={25}>
-// <Card background='#4C8077'>
-//   <div className="project">
-//     <div className="project__header">
-//       <img src="/images/MLG-logo-80.jpg" alt="MLG Logo" />
-//       <h3>Machine Learning Podcast</h3>
-//     </div>
-//     <div className="blurb-container">
-//
-//       <br />
-//
-//
-//       <p className="blurb">
-//         This series aims to teach you the high level fundamentals of machine learning
-//         from A to Z. We discuss languages and frameworks, deep learning, and more.
-//         At the end of every episode I’ll provide the best-of-the-best resources curated from around the web for
-//         you to learn each episode’s details.
-//       </p>
-//       <a href="https://habitica.com" target="_blank">visit</a>
-//     </div>
-//   </div>
-// </Card>
-//
-// <Card background='#C4FCF0'>
-//   <div className="project">
-//     <div className="project__header">
-//       <img src="/images/habitica-logo-80.png" alt="Habitica Logo" />
-//       <h3>Habitica</h3>
-//     </div>
-//
-//     <div className="blurb-container">
-//
-//         <br />
-//
-//
-//       <p className="blurb">
-//          I built the website, mobile apps, and Chrome Extension.
-//          Managed dev-ops on Heroku and Amazon Web Services.
-//          This was a deep-dive into scalability, marketing, fundraising, hiring, and more.
-//          I managed an open source community of 200+ contributors.
-//          Was a one-man-band running this 800k+ user-base for 1.5y before hiring.
-//
-//       </p>
-//       <a href="https://habitica.com" target="_blank">visit</a>
-//     </div>
-//   </div>
-// </Card>
-// <Card background='#4C8077'>
-//   <div className="project">
-//     <div className="project__header">
-//       <img src="/images/OCDevel-logo-sq-80.jpg" alt="OCDevel Logo" />
-//       <h3>Other OCDevel Projects</h3>
-//     </div>
-//       <div className="blurb-container OCD">
-//
-//         <br />
-//
-//
-//       <p className="blurb">
-//         -Bitcoin Trading Bot <br />
-//       -ExecVision <br />
-//     -Jobpig <br />
-//   -Humana <br />
-//     </p>
-//
-//     </div>
-//   </div>
-// </Card>
-// </CardStack>
+
+
+
+
+
+
+
+// <Collapsible trigger="Machine Learning Guide Podcast">
+//   <p className='blurb'>  This series aims to teach you the high level fundamentals of machine learning from A to Z. We discuss languages and frameworks, deep learning, and more. At the end of every episode I’ll provide the best-of-the-best resources curated from around the web for you to learn each episode’s details.
+//    </p>
+// </Collapsible>
+// <Collapsible trigger="Habitica">
+//   <p className='blurb'>   I built the website, mobile apps, and Chrome Extension.
+//      Managed dev-ops on Heroku and Amazon Web Services.
+//      This was a deep-dive into scalability, marketing, fundraising, hiring, and more.
+//      I managed an open source community of 200+ contributors.
+//      Was a one-man-band running this 800k+ user-base for 1.5y before hiring.
+//    </p>
+// </Collapsible>
+// <Collapsible trigger="Other Projects">
+  <p className='blurb'>
+    -Bitcoin Trading Bot <br />
+    -ExecVision <br />
+    -Jobpig <br />
+    -Humana <br />
+  </p>
+// </Collapsible>
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ const Title = () => (
   <div className="stacked-headers">
       <h2> OCDevel </h2>
       <h1 className="stacked__name">Machine Learning Developer</h1>
-      <h1>Creator of the Machine Learning Guide Podcast</h1>
+      <h1>Creator of the <a href="/mlg">Machine Learning Guide Podcast</a></h1>
   </div>
 );
 
